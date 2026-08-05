1c50678c43a727e598c9b85e245e86bca28da4b8
CurtisMabika <bezauna@googlemail.com>
2026-08-05 11:43:59 +0200

    Update VoiceAssistant.jsx

--- src/VoiceAssistant.jsx
+++ src/VoiceAssistant.jsx
@@ -258,7 +258,7 @@ export default function VoiceAssistant({ onClose }) {
     }, 400);
     return () => clearTimeout(timer);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  
 
   const fermer = () => {
     window.speechSynthesis.cancel();
