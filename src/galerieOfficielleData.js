5fbe0e4b19e383212c83c8aa2760121d91236b32
CurtisMabika <bezauna@googlemail.com>
2026-08-20 19:09:50 +0200

    Create galerieOfficielleData.js

---
+++ src/galerieOfficielleData.js
@@ -0,0 +1,22 @@
+const galerieOfficielle = [
+  {
+    titre: "Arrivée du Chef de l'État",
+    photos: [
+      // { src: "/officiel-arrivee-1.jpg" },
+    ],
+  },
+  {
+    titre: "Défilé militaire et civil",
+    photos: [
+      // { src: "/officiel-defile-1.jpg" },
+    ],
+  },
+  {
+    titre: "Concert de la Libération",
+    photos: [
+      // { src: "/officiel-concert-1.jpg" },
+    ],
+  },
+];
+
+export default galerieOfficielle;
