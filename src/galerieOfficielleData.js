51a370d12e23927b8e70b3e2028912dbfe60b5dd
CurtisMabika <bezauna@googlemail.com>
2026-08-20 20:27:34 +0200

    Update galerieOfficielleData.js

--- src/galerieOfficielleData.js
+++ src/galerieOfficielleData.js
@@ -1,5 +1,23 @@
 const galerieOfficielle = [
   {
+    titre: "Séminaire à l'intention des élus locaux",
+    photos: [
+      { src: "/seminaire-1.jpeg" },
+      { src: "/seminaire-2.jpeg" },
+      { src: "/seminaire-3.jpeg" },
+      { src: "/seminaire-4.jpeg" },
+      { src: "/seminaire-5.jpeg" },
+      { src: "/seminaire-6.jpeg" },
+      { src: "/seminaire-7.jpeg" },
+      { src: "/seminaire-8.jpeg" },
+      { src: "/seminaire-9.jpeg" },
+      { src: "/seminaire-10.jpeg" },
+      { src: "/seminaire-11.jpeg" },
+      { src: "/seminaire-12.jpeg" },
+      { src: "/seminaire-13.jpeg" },
+    ],
+  },
+  {
     titre: "Arrivée du Chef de l'État",
     photos: [
       // { src: "/officiel-arrivee-1.jpg" },
