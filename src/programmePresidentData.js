006dc35b3c895caf7c27afac0e1166c29718ac48
CurtisMabika <bezauna@googlemail.com>
2026-08-25 10:40:39 +0100

    Create programmePresidentData.js

---
+++ src/programmePresidentData.js
@@ -0,0 +1,157 @@
+const programmePresident = [
+  {
+    date: "Mercredi 27 Août 2026",
+    sousTitre: "Arrivée dans la province de l'Ogooué-Ivindo",
+    items: [
+      {
+        heure: "14h00",
+        titre: "Arrivée à Okandja",
+        details: [
+          "Premier arrêt à Iyoko",
+          "Mise en place du piquet d'honneur",
+          "Accueil par le Gouverneur de la province",
+          "Accueil par le Président du Conseil départemental",
+          "Accueil par le Chef d'état-major particulier du Président de la République",
+          "Présence des responsables militaires régionaux",
+        ],
+      },
+      {
+        heure: "16h00",
+        titre: "Arrivée à Makokou",
+        details: [
+          "Deuxième arrêt au pont Liboumba, lieu historique de la révolte des femmes en 2023",
+          "Accueil en présence des autorités républicaines, parlementaires, militaires et locales",
+          "Départ du cortège présidentiel en direction du Palais présidentiel de Makokou",
+        ],
+      },
+    ],
+  },
+  {
+    date: "Vendredi 28 Août 2026",
+    sousTitre: "Visite et inaugurations dans la province",
+    items: [
+      {
+        heure: "10h00 – 11h30",
+        titre: "Booué",
+        details: [
+          "Départ pour Booué",
+          "Inauguration du commissariat de Booué",
+          "Discours du Maire",
+          "Discours du Commandant en chef de la Police nationale",
+          "Visite des locaux",
+        ],
+      },
+      {
+        heure: "12h00",
+        titre: "Ovan",
+        details: [
+          "Inauguration de la mairie d'Ovan",
+          "Discours du Maire",
+          "Coupure du ruban symbolique",
+          "Visite des locaux",
+        ],
+      },
+      {
+        heure: "14h00",
+        titre: "Mékambo",
+        details: [
+          "Inauguration de la mairie",
+          "Inauguration de la salle polyvalente",
+          "Inauguration du marché",
+          "Discours du Maire",
+          "Discours de la représentante des femmes commerçantes",
+          "Coupure du ruban symbolique",
+          "Visite des différents sites",
+          "Retour du Président de la République à Makokou",
+        ],
+      },
+    ],
+  },
+  {
+    date: "Samedi 29 Août 2026",
+    sousTitre: "Inaugurations et visites à Makokou",
+    items: [
+      {
+        heure: "10h00",
+        titre: "Inauguration du bâtiment administratif",
+        details: ["Honneurs militaires", "Mot de bienvenue du Maire", "Discours du Gouverneur de la province", "Coupure du ruban symbolique", "Visite des locaux"],
+      },
+      {
+        heure: "11h00 – 13h30",
+        titre: "Inaugurations de plusieurs infrastructures",
+        details: [
+          "Inauguration de la BCEG",
+          "Discours du Ministre chargé des Petites et Moyennes Entreprises",
+          "Coupure du ruban symbolique",
+          "Visite des locaux",
+          "Inauguration de l'Hôtel Ivindo Palace",
+          "Discours du Ministre chargé du Tourisme",
+          "Coupure du ruban symbolique",
+          "Visite des lieux",
+        ],
+      },
+      {
+        heure: "14h00 – 15h00",
+        titre: "Inauguration du marché de Makokou",
+        details: ["Discours de la représentante des femmes commerçantes", "Discours du Ministre chargé des Petites et Moyennes Entreprises", "Coupure du ruban symbolique", "Visite du marché"],
+      },
+      {
+        heure: "15h30 – 16h30",
+        titre: "Inauguration du lycée d'excellence",
+        details: ["Discours du Directeur du Prytanée militaire", "Discours de la Ministre d'État chargée de l'Éducation nationale", "Coupure du ruban symbolique", "Visite des locaux"],
+      },
+      {
+        heure: "",
+        titre: "Visite des logements destinés aux fils et filles de la province",
+        details: ["Présentation du projet", "Discours du Ministre chargé de l'Habitat", "Visite des logements"],
+      },
+      {
+        heure: "17h00",
+        titre: "Pose de la première pierre du bâtiment administratif de la société Ivindo Iron",
+        details: ["Cérémonie de pose de la première pierre", "Discours d'engagement de l'opérateur"],
+      },
+      { heure: "18h00", titre: "Fin des activités officielles de la journée", details: [] },
+      {
+        heure: "19h45",
+        titre: "Discours à la Nation du Président de la République",
+        details: ["Diffusion du discours à la Nation", "Décryptage du discours par les médias et des personnalités locales"],
+      },
+    ],
+  },
+  {
+    date: "Dimanche 30 Août 2026",
+    sousTitre: "Célébration de la Fête de la Libération",
+    items: [
+      { heure: "09h00", titre: "Début de la cérémonie officielle", details: [] },
+      {
+        heure: "09h30",
+        titre: "Revue des troupes et honneurs au drapeau",
+        details: [
+          "Revue des troupes",
+          "Honneurs au drapeau",
+          "Arrivée et installation de la Première Dame, Zita Oligui Nguema, à la tribune officielle",
+          "Arrivée et installation du Président de la République à la tribune officielle",
+          "Cérémonie de décoration",
+        ],
+      },
+      {
+        heure: "09h30",
+        titre: "Début du défilé militaire et civil",
+        details: ["Défilé des forces de défense et de sécurité", "Défilé civil", "Parade culturelle mettant à l'honneur les traditions, le patrimoine et la diversité culturelle de la province de l'Ogooué-Ivindo"],
+      },
+      { heure: "14h30", titre: "Fin du défilé", details: [] },
+      { heure: "15h30", titre: "Finale de la Coupe de la Libération", details: ["Finale du tournoi de football de la Coupe de la Libération"] },
+      { heure: "20h00", titre: "Dîner de gala", details: ["Dîner de gala offert par le couple présidentiel à la salle polyvalente de l'Hôtel de Belinga"] },
+      { heure: "03h30", titre: "Fin de la soirée", details: [] },
+    ],
+  },
+  {
+    date: "Lundi 31 Août 2026",
+    sousTitre: "Retour à Libreville",
+    items: [
+      { heure: "", titre: "Retour à Libreville", details: ["Retour du couple présidentiel à Libreville, au terme du séjour officiel dans la province de l'Ogooué-Ivindo"] },
+    ],
+  },
+];
+
+export default programmePresident;
